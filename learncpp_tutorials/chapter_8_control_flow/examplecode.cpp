196ebfb102cea29fa3264f271f623462e4652344
rua0ra1 <icherryme.0407@gmail.com>
2024-09-08 11:51:14 +1000

    completed chapter 8.6

--- learncpp_tutorials/chapter_8_control_flow/examplecode.cpp
+++ learncpp_tutorials/chapter_8_control_flow/examplecode.cpp
@@ -28,6 +28,32 @@ void example2(){
    printDigitName(2);
 }
 
+//example 3 sequential case labels
+/* 
+bool isVowel(char c)
+{
+    return (c=='a' || c=='e' || c=='i' || c=='o' || c=='u' ||
+        c=='A' || c=='E' || c=='I' || c=='O' || c=='U');
+}
+ switch (c)
+    {
+    case 'a': // if c is 'a'
+    case 'e': // or if c is 'e'
+    case 'i': // or if c is 'i'
+    case 'o': // or if c is 'o'
+    case 'u': // or if c is 'u'
+    case 'A': // or if c is 'A'
+    case 'E': // or if c is 'E'
+    case 'I': // or if c is 'I'
+    case 'O': // or if c is 'O'
+    case 'U': // or if c is 'U'
+        return true;
+    default:
+        return false;
+    }
+
+ */
+
 int main(){
     
 }
