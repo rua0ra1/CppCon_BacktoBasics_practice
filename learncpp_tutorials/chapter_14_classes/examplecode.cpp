ac103bdbc98930f7fa3083bfdf8ddecd6a90ff74
rua0ra1 <icherryme.0407@gmail.com>
2024-09-26 11:59:27 +1000

    completed chapter 14

--- learncpp_tutorials/chapter_14_classes/examplecode.cpp
+++ learncpp_tutorials/chapter_14_classes/examplecode.cpp
@@ -245,3 +245,440 @@ int main()
 //         m_x = x; // incorrect: this is an assignment, not an initialization
 //         m_y = y; // incorrect: this is an assignment, not an initialization
 //     }
+
+/* class Animal{
+     
+    public:
+    int id;
+    Animal(){
+        std::cout << "Default Constructor called\n";
+    }
+
+};
+
+int main(){
+    Animal a;
+    std::cout<<"it deafult initialization "<<a.id<<"\n";
+
+    Animal b{};
+    std::cout<<"it value initialization "<<b.id<<"\n";
+    
+
+} */
+
+/* constructor with default arguments */
+// #include <iostream>
+
+// class Foo
+// {
+// private:
+//     int m_x { };
+//     int m_y { };
+
+// public:
+//     // Foo(){
+//     //     std::cout<<"default constructor is called"<<'\n';
+//     // }
+//     Foo(int x, int y=0) // has default arguments
+//         : m_x { x }
+//         , m_y { y }
+//     {
+//         std::cout << "Foo(" << m_x << ", " << m_y << ") constructed\n";
+//     }
+// };
+
+// int main()
+// {
+//     Foo foo1{};     // calls Foo(int, int) constructor using default arguments
+//     Foo foo2{6, 7}; // calls Foo(int, int) constructor
+
+//     return 0;
+// }
+
+// explicit defaulted default constructor
+
+// #include <iostream>
+
+// class Default
+// {
+// private:
+//     int m_a; // note: no default initialization value
+//     int m_b {};
+    
+
+// public:
+//    int*p;
+//    Default()=default; // explicitly defaulted default constructor
+
+//     int a() const { return m_a; }
+//     int b() const { return m_b; }
+// };
+
+// int main(){
+//     Default def{}; // zero initialized, then default initialized
+//     std::cout << def.a() << ' ' << def.b() << *def.p<< '\n';
+
+// }
+
+// only create default constructor when it makes sense
+// #include <iostream>
+
+// class Fraction
+// {
+// private:
+//     int m_numerator{1};
+//     int m_denominator{2};
+
+// public:
+//     Fraction(){}
+//     Fraction(int numerator, int denominator)
+//         : m_numerator{ numerator }
+//         , m_denominator{ denominator }
+//     {
+//     }
+
+//     void print() const
+//     {
+//         std::cout << "Fraction(" << m_numerator << ", " << m_denominator << ")\n";
+//     }
+// };
+
+// int main()
+// {
+//     Fraction f1 {3, 5};
+//     f1.print();
+
+//     Fraction f2 {}; // will get Fraction 0/1
+//     f2.print();
+//     Fraction f3;
+//     f3.print();
+
+//     return 0;
+// }
+
+// #include<iostream>
+// #include<string>
+// #include<string_view>
+
+// class Employee
+// {
+// private:
+//     std::string m_name{};
+//     int m_id{ 0 }; // default member initializer
+
+// public:
+
+//     Employee(std::string_view name, int id = 0) // default argument for id
+//         : m_name{ name }, m_id{ id }
+//     {
+//         std::cout << "Employee " << m_name << " created\n";
+//     }
+// };
+
+// int main()
+// {
+//     Employee e1{ "James" };
+//     Employee e2{ "Dave", 42 };
+// }
+
+// temporary class objects
+/* #include <iostream>
+
+class IntPair
+{
+private:
+    int m_x{};
+    int m_y{};
+
+public:
+    IntPair(int x, int y)
+        : m_x { x }, m_y { y }
+    {}
+
+    int x() const { return m_x; }
+    int y() const { return m_y; }
+};
+
+void print(IntPair p)
+{
+    std::cout << "(" << p.x() << ", " << p.y() << ")\n";
+}
+
+int main()
+{
+    // Case 1: Pass variable
+    IntPair p { 3, 4 };
+    print(p); // prints (3, 4)
+
+    return 0;
+} */ 
+/* #include <iostream>
+
+class IntPair
+{
+private:
+    int m_x{};
+    int m_y{};
+
+public:
+    IntPair(int x, int y)
+        : m_x { x }, m_y { y }
+    {}
+
+    int x() const { return m_x; }
+    int y() const{ return m_y; }
+};
+
+void print(IntPair p)
+{
+    std::cout << "(" << p.x() << ", " << p.y() << ")\n";
+}
+
+int main()
+{
+    // Case 1: Pass variable
+    IntPair p { 3, 4 };
+    print(p);
+
+    // Case 2: Construct temporary IntPair and pass to function
+    print(IntPair { 5, 6 } );
+
+    // Case 3: Implicitly convert { 7, 8 } to a temporary Intpair and pass to function
+    print( { 7, 8 } );
+
+    return 0;
+} */
+
+/* #include <iostream>
+
+class Fraction
+{
+private:
+    int m_numerator{ 0 };
+    int m_denominator{ 1 };
+
+public:
+    // Default constructor
+    Fraction(int numerator = 0, int denominator = 1)
+        : m_numerator{ numerator }, m_denominator{ denominator }
+    {
+    }
+
+    // Copy constructor
+    Fraction(const Fraction& fraction)
+        : m_numerator{ fraction.m_numerator }
+        , m_denominator{ fraction.m_denominator }
+    {
+        std::cout << "Copy constructor called\n";
+    }
+
+    void print() const
+    {
+        std::cout << "Fraction(" << m_numerator << ", " << m_denominator << ")\n";
+    }
+};
+
+void printFraction(Fraction f) // f is pass by value
+{
+    f.print();
+}
+
+Fraction generateFraction(int n, int d)
+{
+    Fraction f{ n, d };
+    return f;
+}
+
+int main()
+{
+    Fraction f2 { generateFraction(1, 2) }; // Fraction is returned using copy constructor
+
+    printFraction(f2); // f2 is copied into the function parameter using copy constructor
+
+    return 0;
+} */
+
+
+/* int main(){
+int a;         // no initializer (default initialization)
+int b = 5;     // initializer after equals sign (copy initialization)
+int c( 6 );    // initializer in parentheses (direct initialization)
+
+// List initialization methods (C++11)
+int d { 7 };   // initializer in braces (direct list initialization)
+int e = { 8 }; // initializer in braces after equals sign (copy list initialization)
+int f {};      // initializer is empty braces (value initialization)
+
+std::cout<< "a: "<< a <<"f "<< f <<std::endl;
+}
+ */
+
+/* #include <iostream>
+#include <string>
+#include <string_view>
+
+class Employee
+{
+private:
+    std::string m_name{};
+
+public:
+    Employee(std::string_view name)
+        : m_name{ name }
+    {
+    }
+
+    const std::string& getName() const { return m_name; }
+};
+
+void printEmployee(Employee e) // has an Employee parameter
+{
+    std::cout << e.getName();
+}
+
+int main()
+{ 
+    using namespace std::literals;
+    printEmployee("Joe"sv); // we're supplying an string literal argument
+ return 0;
+}*/
+
+// the explicit keyword
+/* #include <iostream>
+class Dollars
+{
+private:
+    int m_dollars{};
+
+public:
+     explicit Dollars(int d) // now explicit
+        : m_dollars{ d }
+    {
+    }
+
+    int getDollars() const { return m_dollars; }
+};
+
+void print(Dollars d)
+{
+    std::cout << "$" << d.getDollars();
+}
+
+int main()
+{
+    print(5); // compilation error because Dollars(int) is explicit
+
+    return 0;
+} */
+
+// constexpr aggregates and classes
+/* #include <iostream>
+
+constexpr int greater(int x, int y)
+{
+    return (x > y ? x : y);
+}
+
+int main()
+{
+    std::cout << greater(5, 6);        // greater(5, 6) may be evaluated at compile-time or runtime
+
+    constexpr int g { greater(5, 6) }; // greater(5, 6) must be evaluated at compile-time
+    std::cout << g;                    // prints 6
+
+    return 0;
+} */
+
+// #include <iostream>
+
+// struct Pair
+// {
+//     int m_x {};
+//     int m_y {};
+
+//     constexpr int  greater() const
+//     {
+//         return (m_x > m_y  ? m_x : m_y);
+//     }
+// };
+
+// int main()
+// {
+//     constexpr Pair p { 5, 6 };                 // inputs are constexpr values
+//     std::cout << p.greater();        // p.greater() evaluates at runtime
+
+//     constexpr int g { p.greater() }; // compile error: greater() not constexpr
+//     std::cout << g;
+
+//     return 0;
+// }
+
+// constexpr clas objects and constexpr constructors
+/* 
+#include <iostream>
+
+class Pair // Pair is no longer an aggregate
+{
+private:
+    int m_x {};
+    int m_y {};
+
+public:
+    constexpr Pair(int x, int y): m_x { x }, m_y { y } {}
+
+    constexpr int greater() const
+    {
+        return (m_x > m_y  ? m_x : m_y);
+    }
+};
+
+int main()
+{
+    constexpr Pair p { 5, 6 };       // compile error: p is not a literal type
+    std::cout << p.greater();
+
+    constexpr int g { p.greater() };
+    std::cout << g;
+
+    return 0;
+} */
+
+#include <iostream>
+
+class Pair
+{
+private:
+    int m_x {};
+    int m_y {};
+
+public:
+    constexpr Pair(int x, int y): m_x { x }, m_y { y } {}
+
+    constexpr int greater() const // constexpr and const
+    {
+        return (m_x > m_y  ? m_x : m_y);
+    }
+
+    constexpr void reset() // constexpr but not const
+    {
+        m_x = m_y = 0;
+    }
+
+    constexpr const int& getX() const { return m_x; }
+};
+
+// This function is constexpr
+constexpr Pair zero()
+{
+    Pair p { 1, 2 }; // p is non-const
+    p.reset();       // call constexpr member function on non-const object
+    return p;
+}
+
+int main()
+{
+    constexpr Pair p2 { zero() }; // zero() must evaluate at compile-time
+    std::cout << p2.getX();       // prints 0
+
+    return 0;
+}
