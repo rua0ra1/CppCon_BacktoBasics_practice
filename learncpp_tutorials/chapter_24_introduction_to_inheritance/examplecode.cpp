1cdbd67d1948afded5a9782b3e495700c4f361ce
rua0ra1 <icherryme.0407@gmail.com>
2024-10-02 16:38:37 +1000

    completed inheritance

--- learncpp_tutorials/chapter_24_introduction_to_inheritance/examplecode.cpp
+++ learncpp_tutorials/chapter_24_introduction_to_inheritance/examplecode.cpp
@@ -123,7 +123,7 @@ public:
     double getCost() const { return m_cost; }
 }; */
 
-#include <iostream>
+/* #include <iostream>
 
 class Base
 {
@@ -165,3 +165,154 @@ int main() {
      Derived derived{ 1.3, 5 }; // use Derived(double, int) constructor
     std::cout << "Id: " << derived.getId() << '\n';
     std::cout << "Cost: " << derived.getCost() << '\n';}
+ */
+
+// adding new functionality to a derived class
+/* #include <iostream>
+
+class Base
+{
+protected:
+    int m_value {};
+
+public:
+    Base(int value)
+        : m_value { value }
+    {
+    }
+
+    void identify() const { std::cout << "I am a Base\n"; }
+};
+
+class Derived: public Base
+{
+public:
+    Derived(int value)
+        : Base { value }
+    {
+    }
+
+    int getValue() const { return m_value; }
+};
+
+int main()
+{
+    Derived derived { 5 };
+    std::cout << "derived has value " << derived.getValue() << '\n';
+
+    return 0;
+} */
+
+/* #include <iostream>
+
+class Base
+{
+public:
+    Base() { }
+
+     void identify() const { std::cout << "Base::identify()\n"; }
+};
+
+class Derived: public Base
+{
+public:
+    Derived() { }
+
+    void identify() const {std::cout << "derived::identify()\n"; }
+};
+
+int main()
+{
+    Base base {};
+    base.identify();
+
+    Derived derived {};
+    derived.identify();
+
+    return 0;
+} */
+
+// hiding functionality
+
+/* #include<iostream>
+
+class Base{
+    public:
+    int m_value{30};
+
+};
+
+class Derived : public Base
+{
+
+
+public:
+     int m_value=20;
+};
+
+int main(){
+    Derived d;
+    
+}
+ */
+// Deleting functions in the derived class
+/* #include <iostream>
+class Base
+{
+private:
+	int m_value {};
+
+public:
+	Base(int value)
+		: m_value { value }
+	{
+	}
+
+	int getValue() const { return m_value; }
+};
+
+class Derived : public Base
+{
+public:
+	Derived(int value)
+		: Base { value }
+	{
+	}
+
+
+	int getValue() const = delete; // mark this function as inaccessible
+};
+
+int main()
+{
+	Derived derived { 7 };
+
+    Base b(10);
+
+	// The following won't work because getValue() has been deleted!
+	std::cout << static_cast<Base>(derived).getValue();
+
+	return 0;
+} */
+
+#include <iostream>
+
+class Base {
+public:
+    virtual int getValue() const { return 10; } // Base method
+};
+
+class Derived : public Base {
+public:
+    int getValue() const { return 20; } // Overridden method
+};
+
+int main() {
+    Derived derived;
+
+    // Using static_cast to cast derived to Base reference
+    std::cout << static_cast<Base&>(derived).getValue() << std::endl; // Outputs 20
+
+    return 0;
+}
+
