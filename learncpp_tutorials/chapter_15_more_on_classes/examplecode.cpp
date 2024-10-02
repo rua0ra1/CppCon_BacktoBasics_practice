a08f1867a4c0ccd7b0e4a9136f85334368b6109a
rua0ra1 <icherryme.0407@gmail.com>
2024-10-02 15:13:28 +1000

    completed chapter 15

--- learncpp_tutorials/chapter_15_more_on_classes/examplecode.cpp
+++ learncpp_tutorials/chapter_15_more_on_classes/examplecode.cpp
@@ -33,7 +33,7 @@ int main()
 } */
 
 /* nested types (member types) */
-
+/* 
 #include <iostream>
 
 
@@ -78,4 +78,100 @@ int main(){
 		std::cout << "I am not an apple";
 
 	return 0;
+}; */
+
+// Static member variables
+/* #include<iostream>
+
+struct Something{
+  const static int s_value{2}; //now static
 };
+
+//int Something::s_value{1};
+
+int main(){
+    Something first{};
+    Something second{};
+
+    first.s_value=2;
+
+    std::cout<<first.s_value<<'\n';
+    std::cout<<second.s_value<<'\n';
+
+
+} */
+
+/* #include <iostream>
+
+class Something{
+
+    private:
+    static int s_value;
+
+    public:
+    static int getValue(){return s_value;} // static member function
+
+};
+
+int Something::s_value {2};
+
+
+int main(){
+    std::cout<<Something::getValue()<<'\n';
+} */
+
+// 
+/* #include <iostream>
+
+struct Chars
+{
+    char first{};
+    char second{};
+    char third{};
+    char fourth{};
+    char fifth{};
+};
+
+class MyClass
+{
+private:
+    static Chars generate()
+    {
+        Chars c{}; // create an object
+        c.first = 'a'; // fill it with values however you like
+        c.second = 'e';
+        c.third = 'i';
+        c.fourth = 'o';
+        c.fifth = 'u';
+
+        return c; // return the object
+    }
+
+public:
+	static inline Chars s_mychars { generate() }; // copy the returned object into s_mychars
+};
+
+int main()
+{
+    std::cout << MyClass::s_mychars.third; // print i
+
+    return 0;
+} */
+#include<iostream>
+class Animal{
+    private:
+    int m_id;
+    public:
+    Animal(int id):m_id(id){}
+    void Addnum(Animal& a_in){
+        std::cout<<"sum is "<<(this->m_id+a_in.m_id)<<'\n';
+
+    }
+};
+
+int main(){
+    Animal a1(2);
+    Animal a2(3);
+
+    a1.Addnum(a2);
+}
---
+++ learncpp_tutorials/chapter_15_more_on_classes/linline_example/inline_variables.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+struct S{
+    const static std::string s;
+};
+
+const inline std::string S::s ="hello";
+
+int main(){
+
+}
