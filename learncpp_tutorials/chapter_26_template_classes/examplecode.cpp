57f70bdefdbe3676e3cec5dd92a5df6561692b53
rua0ra1 <icherryme.0407@gmail.com>
2024-10-09 12:16:42 +1100

    completed till 26.5

---
+++ learncpp_tutorials/chapter_26_template_classes/examplecode.cpp
@@ -0,0 +1,191 @@
+/* #include <iostream>
+
+template <typename T, int size> // size is an integral non-type parameter
+class StaticArray
+{
+private:
+    // The non-type parameter controls the size of the array
+    T m_array[size] {};
+
+public:
+    T* getArray();
+
+    T& operator[](int index)
+    {
+        return m_array[index];
+    }
+};
+
+// Showing how a function for a class with a non-type parameter is defined outside of the class
+template <typename T, int size>
+T* StaticArray<T, size>::getArray()
+{
+    return m_array;
+}
+
+int main()
+{
+    // declare an integer array with room for 12 integers
+    StaticArray<int, 12> intArray;
+
+    // Fill it up in order, then print it backwards
+    for (int count { 0 }; count < 12; ++count)
+        intArray[count] = count;
+
+    for (int count { 11 }; count >= 0; --count)
+        std::cout << intArray[count] << ' ';
+    std::cout << '\n';
+
+    // declare a double buffer with room for 4 doubles
+    StaticArray<double, 4> doubleArray;
+
+    for (int count { 0 }; count < 4; ++count)
+        doubleArray[count] = 4.4 + 0.1 * count;
+
+    for (int count { 0 }; count < 4; ++count)
+        std::cout << doubleArray[count] << ' ';
+
+    return 0;
+} */
+
+// instantiate a template non type parameter with a non-constexpr value
+/* template<int size>
+
+class Foo{
+
+};
+
+int main(){
+     int x{ 4 }; // x is non-constexpr
+    Foo<x> f; // error: the template non-type argument must be constexpr
+
+    return 0;
+} */
+
+// using non template funciton
+/* #include<iostream>
+
+template<typename T>
+void print(const T& t){
+    std::cout<< t <<'\n';
+}
+
+int main()
+{
+    print(5);
+    print(6.7);
+
+    return 0;
+} */
+
+// function template specialization 
+/* #include<iostream>
+
+template<typename T>
+void print (const T& t){
+    std::cout<< t<<'\n';
+}
+
+// A full specialization of primary template print<T> for type double
+// Full specializations are not implicitly inline, so make this inline if put in header file
+template<>                          // template parameter declaration containing no template parameters
+void print<double>(const double& d) // specialized for type double
+{
+    std::cout << std::scientific << d << '\n';
+}
+
+int main(){
+    print(5);
+    print(6.7);
+}
+ */
+
+// class function specialization
+#include <cstdint>
+#include<iostream>
+
+// First define our non-specialized class template
+template <typename T>
+class Storage8
+{
+private:
+    T m_array[8];
+
+public:
+    void set(int index, const T& value)
+    {
+        m_array[index] = value;
+    }
+
+    const T& get(int index) const
+    {
+        return m_array[index];
+    }
+};
+
+// Now define our specialized class template
+template <> // the following is a template class with no templated parameters
+class Storage8<bool> // we're specializing Storage8 for bool
+{
+// What follows is just standard class implementation details
+
+private:
+    std::uint8_t m_data{};
+
+public:
+    // Don't worry about the details of the implementation of these functions
+    void set(int index, bool value)
+    {
+        // Figure out which bit we're setting/unsetting
+        // This will put a 1 in the bit we're interested in turning on/off
+        auto mask{ 1 << index };
+
+        if (value)  // If we're setting a bit
+            m_data |= mask;   // use bitwise-or to turn that bit on
+        else  // if we're turning a bit off
+            m_data &= ~mask;  // bitwise-and the inverse mask to turn that bit off
+	}
+
+    bool get(int index)
+    {
+        // Figure out which bit we're getting
+        auto mask{ 1 << index };
+        // bitwise-and to get the value of the bit we're interested in
+        // Then implicit cast to boolean
+        return (m_data & mask);
+    }
+};
+
+// Same example as before
+int main()
+{
+    // Define a Storage8 for integers (instantiates Storage8<T>, where T = int)
+    Storage8<int> intStorage;
+
+    for (int count{ 0 }; count < 8; ++count)
+    {
+        intStorage.set(count, count);
+	}
+
+    for (int count{ 0 }; count < 8; ++count)
+    {
+        std::cout << intStorage.get(count) << '\n';
+    }
+
+    // Define a Storage8 for bool  (instantiates Storage8<bool> specialization)
+    Storage8<bool> boolStorage;
+
+    for (int count{ 0 }; count < 8; ++count)
+    {
+        boolStorage.set(count, count & 3);
+    }
+
+	std::cout << std::boolalpha;
+
+    for (int count{ 0 }; count < 8; ++count)
+    {
+        std::cout << boolStorage.get(count) << '\n';
+    }
+
+    return 0;
+}
